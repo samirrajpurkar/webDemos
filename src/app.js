db66ee1e54412659f0ee5acca902f23346f3b3a7
samirrajpurkar <samir.rajpurkar@gmail.com>
2016-12-28 14:58:08 +0400

    Add .eslintrc, babel npm and seperated main and execution layes

--- src/app.js
+++ src/app.js
@@ -1,41 +1,33 @@
-import $ from 'jquery'
-import Rx from 'rxjs/Rx'
+import $ from 'jquery';
+import Rx from 'rxjs/Rx';
 
 function main() {
-
   return {
     DOM: Rx.Observable.timer(0, 1000) // 0 - 1 - 2 - 3 - 4
           .map(i => `Seconds elapsed ${i}`),
     Log: Rx.Observable.timer(0,2000) // 0 -- 1 -- 2 -- 3
           .map(i => 2 * i)
-  }
+  };
 }
 
 //Effects (imperatives)
 function DOMEffect(text$) {
   text$.subscribe(
     text => {
-      const app = document.querySelector('#app')
-      app.textContent = text
+      const app = document.querySelector('#app');
+      app.textContent = text;
     }
-  )
+  );
 }
 
 function consoleLogEffect(msg$) {
   msg$.subscribe(
     msg => {
-      console.log(msg)
+      console.log(msg);
     }
-  )
+  );
 }
 
-const sink = main()
-DOMEffect(sink.DOM)
-consoleLogEffect(sink.Log)
-
-
-
-
-
-
-
+const sink = main();
+DOMEffect(sink.DOM);
+consoleLogEffect(sink.Log);
