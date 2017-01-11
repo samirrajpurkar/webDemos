1938b3d60255f94f57d0852727c0b03c04d14d4f
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-01-11 13:59:21 +0400

    understand jquery promises

--- src/async.js
+++ src/async.js
@@ -63,41 +63,56 @@
 //     error: handleError
 //   });
 // };
-window.onload = function ( ) {
-  function get(url) {
-    return new Promise(function (resolve, reject) {
-      var xhttp = new XMLHttpRequest();
-      xhttp.open('GET', url, true);
-      xhttp.onload = function ( ) {
-        if (xhttp.status === 200) {
-          resolve(JSON.parse(xhttp.response));
-        }
-        else {
-          reject(xhttp.statusText);
-        }
-      };
-      xhttp.onerror = function ( ) {
-        reject(xhttp.statusText);
-      };
-      xhttp.send();
-    });
-  }
 
-  var promise = get('./data/tweets.json');
-  promise.then(function (tweets) {
-    console.log(tweets);
-    return get('./data/friends.json');
-  }).then(function (friends) {
-    console.log(friends);
-    return get('./data/videos.json');
-  }).then(function (videos) {
-    console.log(videos);
-  }).catch(function (error) {
-    console.log('ERROR getting videos', error);
-  }).catch(function (error) {
-    console.log('ERROR getting friends', error);
-  }).catch(function (error) {
-    console.log('ERROR in getting Tweets : ', error);
-  });
+// window.onload = function ( ) {
+//   function get(url) {
+//     return new Promise(function (resolve, reject) {
+//       var xhttp = new XMLHttpRequest();
+//       xhttp.open('GET', url, true);
+//       xhttp.onload = function ( ) {
+//         if (xhttp.status === 200) {
+//           resolve(JSON.parse(xhttp.response));
+//         }
+//         else {
+//           reject(xhttp.statusText);
+//         }
+//       };
+//       xhttp.onerror = function ( ) {
+//         reject(xhttp.statusText);
+//       };
+//       xhttp.send();
+//     });
+//   }
+
+//   var promise = get('./data/tweets.json');
+//   promise.then(function (tweets) {
+//     console.log(tweets);
+//     return get('./data/friends.json');
+//   }).then(function (friends) {
+//     console.log(friends);
+//     return get('./data/videos.json');
+//   }).then(function (videos) {
+//     console.log(videos);
+//   }).catch(function (error) {
+//     console.log('ERROR getting videos', error);
+//   }).catch(function (error) {
+//     console.log('ERROR getting friends', error);
+//   }).catch(function (error) {
+//     console.log('ERROR in getting Tweets : ', error);
+//   });
+// };
+window.onload = function ( ) {
+  $.get('./data/tweets.json')
+    .then(function (tweets) {
+      console.log(tweets);
+      return $.get('./data/friends.json');
+    })
+     .then(function (friends) {
+       console.log(friends);
+       return $.get('./data/videos.json');
+     })
+      .then(function (videos) {
+        console.log(videos);
+      });
 };
 
