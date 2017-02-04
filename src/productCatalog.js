2b2c616bc44f3ee5949bf8b17a7f4ebcc9c9b87f
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-02-04 21:27:03 +0400

    updateTable

--- src/productCatalog.js
+++ src/productCatalog.js
@@ -12,5 +12,49 @@ var createTableHeader = function (tableId) {
   tableHeaderRow.appendChild(th2);
   tableHeaderRow.appendChild(th3);
   tableHeaderRow.appendChild(th4);
-  document.getElementById(tableHeaderRow).appendChild(tableHeaderRow);
+  document.getElementById(tableId).appendChild(tableHeaderRow);
+};
+
+var updateTable = function (tableId, productArray) {
+  // take a reference to table element
+  var tableBody = document.getElementById(tableId);
+  //reset table
+  while (tableBody.hasChildNodes()) {
+    tableBody.removeChild(tableBody.firstChild);
+  }
+  // create table header
+  createTableHeader(tableId);
+  // populate table rows
+  for (var i = 0; i < productArray.length; i++) {
+    var tr = document.createElement('TR');
+    var td1 = document.createElement('TD');
+    var td2 = document.createElement('TD');
+    var td3 = document.createElement('TD');
+    var td4 = document.createElement('BUTTON');
+
+    td4.addEventListener('click', function () { });
+
+    td1.appendChild(document.createTextNode(productArray[i].id));
+    td2.appendChild(document.createTextNode(productArray[i].type));
+    td3.appendChild(document.createTextNode(productArray[i].price));
+    td4.appendChild(document.createTextNode('Examine'));
+
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
+
+    tableBody.appendChild(tr);
+  }
+};
+
+api.searchAllProducts().then(function (products) {
+  updateTable('allTable', products);
+});
+
+var updateExaminedText = function (product) {
+  var outputString = 'Product Id: ' + product.id;
+  outputString += '<br> Price : ' + product.price;
+  outputString += '<br> Type : ' + product.type;
+  document.getElementById('productText').innerHTML = outputString;
 };
