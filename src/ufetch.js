87cdca61dda05b012925557ae7ff1107ca4eeb31
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-02-09 03:57:54 +0400

    fetching a bad url

--- src/ufetch.js
+++ src/ufetch.js
@@ -8,3 +8,21 @@ fetch('https://jsonplaceholder.typicode.com/todos/1')
   .catch(function (error) {
     console.log(error);
   });
+
+  // fetching a bad url
+fetch('https://jsonplaceholder.typicode.com/bad_url/1')
+    .then(function (result) {
+      console.log(result);
+      if (result.ok) {
+        return result.text();
+      }
+      else {
+        return Promise.reject(result.status);
+      }
+    })
+    .then(function (result) {
+      console.log(result);
+    })
+    .catch(function (error) {
+      console.log('Error while fetching the URL ' + error);
+    });
