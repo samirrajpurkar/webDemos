ff0c6f1badf5fe97571dc3689a71738eb5af05c2
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-02-20 19:43:19 +0400

    pre-compiled templates and cache them using closure

---
+++ src/jquerypubsub.js
@@ -0,0 +1,5 @@
+(function ($) {
+  // Pre-compiled templates and cache them using closure
+  var userTemplate = _.template($('#userTemplate').html());
+  var ratingsTemplate = _.template($('#ratingsTemplate').html());
+})(jQuery);
