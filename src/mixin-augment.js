88fdac2b6c09acf54e8b407e9308f18abc5a027c
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-03-09 19:56:03 +0400

    Mixin prototype

--- src/mixin-augment.js
+++ src/mixin-augment.js
@@ -10,3 +10,19 @@ Car.prototype.selfDriving = function () {
 
 myMerz = new Car({model: 'Merz', color: 'grey'});
 console.log('Simple Car Instance', myMerz, 'directly calling a prototype method on myMerz', myMerz.selfDriving());
+
+// Mixin
+var Mixin = function () {};
+
+Mixin.prototype = {
+  driveForward: function () {
+    console.log('drive forward');
+  },
+  driveBackward: function () {
+    console.log('drive backward');
+  },
+  driveSideways: function () {
+    console.log('drive sideways');
+  }
+};
+
