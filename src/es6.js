0ca04daacbc03b9296752ba6026c54bee3e7e918
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-03-16 20:27:57 +0400

    let keyword in ES6

--- src/es6.js
+++ src/es6.js
@@ -36,3 +36,53 @@ var e_deliveryBoy = {
 };
 
 e_deliveryBoy.receive();
+
+// The let keyword in ES6
+var fs = [];
+for (var i = 0; i < 10 ; i++) {
+  fs.push(function () {
+    console.log(i);
+  });
+}
+fs.forEach(function (f) {
+  f();
+});
+
+var efs = [];
+for (let j = 0; j < 10 ; j++) {
+  efs.push(function () {
+    console.log(j);
+  });
+}
+efs.forEach(function (f) {
+  f();
+});
+
+function varFunc() {
+  var previous = 0;
+  var current = 1;
+  var i;
+  var temp;
+
+  for (i = 0; i < 10; i += 1) {
+    temp = previous;
+    previous = current;
+    current = temp + current;
+  }
+  return current;
+}
+
+function letFunc() {
+  let previous = 0;
+  let current = 1;
+
+  for (let j = 0; j < 10; j += 1) {
+    let temp = previous;
+    previous = current;
+    current = temp + current;
+  }
+  return current;
+}
+
+console.log('varFunc', varFunc());
+console.log('letFunc', letFunc());
