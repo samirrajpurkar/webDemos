f20148d7b2f2520a823a02e77aae1cd33e63fdbb
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-03-16 20:07:34 +0400

    Interface Decorator

--- src/interfaceDecorator.js
+++ src/interfaceDecorator.js
@@ -12,7 +12,7 @@ var Interface = function (name, methods) {
       throw new Error('Interface constructor expects method names to be ' +
                       'passed in as string.');
     }
-    this.methods.push(method[i]);
+    this.methods.push(methods[i]);
   }
 };
 
@@ -25,6 +25,7 @@ Interface.ensureImplements = function (object) {
   }
   for (var i = 0; i < arguments.length; i++) {
     var interFace = arguments[i];
+    console.log('Interface : ', interFace);
     if (interFace.constructor !== Interface) {
       throw new Error('Function Interface.ensureImplements expects arguments ' +
                       ' two and above to be instances of Interface.');
@@ -42,3 +43,45 @@ Interface.ensureImplements = function (object) {
     }
   }
 };
+
+// Create interfaces using a pre-defined Interface
+// constructor that accepts an interface name and
+// skeleton methods to expose.
+
+// In our reminder example summary() and placeOrder()
+// represent functionality that the interface should
+// support
+var reminder = new Interface('List', ['summary', 'placeOrder']);
+
+var properties = {
+  name: 'Remember to buy the milk',
+  date: '05/06/2016',
+  actions: {
+    summary: function () {
+      return 'Remember to buy the milk, we are almost out!';
+    },
+    placeOrder: function () {
+      return 'Ordering milk from your local grocery store';
+    }
+  }
+};
+
+// Now create a constructor implementing the above properties
+// and methods
+
+var Todo = function (config) {
+  // State the methods we expect to be supported
+  // as well as the Interface instance being checked
+  // against
+
+  //Interface.ensureImplements(config.actions, reminder);
+  this.name = config.name;
+  this.methods = config.actions;
+};
+// Create a new instance of out Todo constructor
+
+var todoItem = new Todo(properties);
+
+// Finally test to make sure these function correctly
+console.log(todoItem.methods.summary());
+console.log(todoItem.methods.placeOrder());
