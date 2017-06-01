489655c50d23e4a404cf8b85201e34f0a8a50f21
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-06-01 23:26:30 +0400

    click counter

--- src/gettingStartedWithRedux.js
+++ src/gettingStartedWithRedux.js
@@ -8,4 +8,24 @@ const counter = (state = 0, action) => {
     return state;
   }
 };
-console.log(expect);
+
+const { createStore } = Redux;
+const store = createStore(counter);
+
+const render = () => {
+  document.body.innerHTML = (store.getState());
+};
+
+store.subscribe(render);
+render();
+
+document.addEventListener('click', () => {
+  store.dispatch({type: 'INCREMENT'});
+}
+);
+
+expect(counter(0,{type: 'Something'})).toEqual(0);
+expect(counter(0, {type: 'INCREMENT'})).toEqual(1);
+expect(counter(0, {type: 'DECREMENT'})).toEqual(-1);
+
+console.log('All test passed');
