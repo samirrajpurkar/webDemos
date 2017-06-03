d0e73f0e391cb72610c43e5af7aa726ae1538b94
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-06-03 10:48:50 +0400

    testAddToDo and testToggleTodoViaReducer

--- src/gettingStartedWithRedux.js
+++ src/gettingStartedWithRedux.js
@@ -139,9 +139,126 @@ const testToggleTodo = () => {
   expect(toggleTodo(todoBefore)).toEqual(todoAfter);
 };
 
+// -----------------------------------
+const todo = (state = [], action) => {
+  switch (action.type) {
+  case 'ADD_TODO':
+    return {id: action.id, text: action.text, completed: false};
+  case 'TOGGLE_TODO':
+    if (state.id !== action.id) {
+      return state;
+    }
+    return {
+      ...state,
+      completed: !state.completed
+    };
+  default:
+    return state;
+  }
+};
+const todos = (state = [], action) => {
+  switch (action.type) {
+  case 'ADD_TODO':
+    return [
+      ...state, todo(undefined, action)
+    ];
+  case 'TOGGLE_TODO':
+    return state.map(t => todo(t, action));
+  default:
+    return state;
+  }
+};
+
+const storeTodos = createStore(todos);
+
+console.log('Initial state');
+console.log(storeTodos.getState());
+console.log('-------------');
+
+console.log('Dispatching ADD_TODO');
+storeTodos.dispatch({
+  type: 'ADD_TODO',
+  id: 0,
+  text: 'Learn Redux'
+});
+console.log('Current State:');
+console.log(storeTodos.getState());
+console.log('-------------');
+
+console.log('Dispatching ADD_TODO');
+storeTodos.dispatch({
+  type: 'ADD_TODO',
+  id: 1,
+  text: 'Clean the house'
+});
+console.log('Current State:');
+console.log(storeTodos.getState());
+console.log('-------------');
+
+// -----------------------------------
+const testAddToDo = () => {
+  const stateBefore = [];
+  const action = {
+    id: 0,
+    text: 'Testing add to do',
+    type: 'ADD_TODO'
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Testing add to do',
+      completed: false
+    }
+  ];
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(todos(stateBefore, action)).toEqual(stateAfter);
+};
+
+const testToggleTodoViaReducer = () => {
+  const stateBefore = [
+    {
+      id: 0,
+      text: 'Complete this lesson',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Clean up the house',
+      completed: false
+    }
+  ];
+
+  const action = {
+    id: 1,
+    type: 'TOGGLE_TODO'
+  };
+
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Complete this lesson',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Clean up the house',
+      completed: true
+    }
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(todos(stateBefore, action)).toEqual(stateAfter);
+};
+
 testAddCounter();
 testRemoveCounter();
 testIncrementCounter();
 testToggleTodo();
+testAddToDo();
+testToggleTodoViaReducer();
 
 console.log('All test passed');
